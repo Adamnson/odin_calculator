318ffce19717f377786acc441e47542a5f165abe
Adamnson-0dinson <theawakeavatar@gmail.com>
2024-03-11 12:05:34 +0530

    (major) add life to calculator
    the numbers can be operated upon,
    the decimal point logic has been implemented
    backspace logic implemented
    cosmetic - buttons rounded but stling onclick not working
    
    }%{ percentage button logic to be designed
    }%{  +/- button logic not in place

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const firstNumber = 3;
-const secondNumber = 5;
-const operator = '+';
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = '';
+let overwriteFlag = 0;
 
 
 function operate (n1, n2, sign)
@@ -23,16 +24,59 @@ function operate (n1, n2, sign)
             console.log("Operation not valid");
     }
     console.log(ans);
+    return ans;
 }
 
 function addToDisplay(value) {
-    document.getElementById('display').value += value;
+    let dispVal = document.getElementById('display').value;
+    if (overwriteFlag) {clearDisplay();}
+    if ( (value != '.') || ((value === '.') && !dispVal.includes('.') )) {
+        document.getElementById('display').value += value;
+        console.log(document.getElementById('display').value);
+    }
 }
 
   function clearDisplay() {
     document.getElementById('display').value = '';
+    if (overwriteFlag) { overwriteFlag = 0;}
 }
 
+function backspace() {
+    let dispVal = document.getElementById('display').value;
+    dispVal = dispVal.substring(0, dispVal.length-1);
+    document.getElementById('display').value = dispVal;
+}
+
+function pushOperate(symbol) {
+    push();
+    switch (symbol) {
+        case '/':
+            operator = '/';
+            break;
+        case 'x':
+            operator = 'x';
+            break;
+        case '-':
+            operator = '-';
+            break;
+        case '+':
+            operator = '+';
+            break;
+    }
+    console.log("opeartor :", operator);
+}
+
+function push() {
+   firstNumber = parseFloat(document.getElementById('display').value);
+   console.log("first number :", firstNumber);
+   overwriteFlag = 1;
+}
+
+function calculate() {
+    secondNumber = parseFloat(document.getElementById('display').value);
+    document.getElementById('display').value = operate(firstNumber, secondNumber, operator);
+    overwriteFlag = 1;
+}
 
 function add(a, b) {
     return a+b;
